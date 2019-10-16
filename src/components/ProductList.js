0ec119791ca267504b85dd2cf682542ec78cc40c
abanoubmesiha <bony.nagy@gmail.com>
2019-10-16 05:44:44 +0200

    finished except cv link

--- src/components/ProductList.js
+++ src/components/ProductList.js
@@ -6,9 +6,22 @@ export default class ProductList extends Component {
     render() {
         return (
             <React.Fragment>
-                <ProductConsumer>
-                    {(value)=> {return <div>{value.map(product => (<Product product={product} key={product.id} />))}</div>;}} 
+               <div className="container my-5">
+                 {/*title */}
+                     <div className="row">
+                        <div className="col-10 mx-auto col-md-6 text-center text-uppercase mb-3">
+                            <h1 className="text-slanted">Product list</h1>
+                        </div>
+                    </div>
+                    {/*end of title*/}
+                    <div className="row">
+                    
+                    <ProductConsumer>
+                     {(value)=> {return <div>{value.map(product => (<Product product={product} key={product.id} />))}</div>;}} 
                 </ProductConsumer>
+                    </div>
+                </div>
+                
             </React.Fragment> 
         )
     }
